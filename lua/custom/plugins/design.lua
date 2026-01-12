2653642b8675b55aa7ce580408ad97eca03d5dab
rvardiashvili <vardiashvilirati33@gmail.com>
2026-01-12 04:52:16 +0400

    Created personal nvim config

--- lua/custom/plugins/design.lua
+++ lua/custom/plugins/design.lua
@@ -4,6 +4,21 @@ return {
     name = 'catppuccin',
     priority = 1000,
     config = function()
+      require('catppuccin').setup {
+        integrations = {
+          cmp = true,
+          gitsigns = true,
+          neotree = true,
+          telescope = true,
+          notify = true, -- Used by Noice
+          mini = {
+            enabled = true,
+          },
+          treesitter = true,
+          mason = true,
+          which_key = true,
+        },
+      }
       vim.cmd.colorscheme 'catppuccin-mocha'
     end,
   },
